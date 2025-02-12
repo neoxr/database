2d84d24f91d953bdde9b29fcd517b74954f0e450
neoxr <admin@neoxr.my.id>
2025-02-12 11:02:24 +0000

    update: connection

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const createDatabase = async (uri = '', db_name = 'database', col_name = 'data')
 
    try {
       client = new MongoClient(uri, {
-         poolSize: 20,
          useNewUrlParser: true,
          useUnifiedTopology: true,
          serverSelectionTimeoutMS: 5000
